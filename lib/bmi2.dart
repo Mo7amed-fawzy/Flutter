7d156f7fc6c8192b806dee0eae0219703aef8ed2
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-03-16 13:32:45 +0200

    first uploud

---
+++ lib/bmi2.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class bmi2 extends StatelessWidget {
+  final int result;
+  final bool isMale;
+  final int age;
+
+  const bmi2({
+    super.key,
+    required this.result,
+    required this.isMale,
+    required this.age,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(221, 27, 26, 26),
+      //backgroundColor: const Color.fromARGB(221, 27, 26, 26),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(221, 27, 26, 26),
+        leading: IconButton(
+            color: Colors.white,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+        title: const Text(
+          'BMI CALCULATOR',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'Your Result',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: Container(
+              height: 500,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.black54,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '${isMale ? 'Male' : 'Female'} ',
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 188, 188, 190),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Result normal : $result',
+                    style: const TextStyle(
+                      color: Colors.lightGreen,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    '$age',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  RichText(
+                    text: const TextSpan(
+                      text: 'Normal BMI range : 18.5-24.9kg',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 168, 167, 167),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '²',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            textBaseline: TextBaseline.alphabetic,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Text(
+                    'You have a normal Body Weight. Good job',
+                    style: TextStyle(
+                      color: Colors.lightGreen,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 59,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      backgroundColor: Colors.purple,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'Re-Calculate Your BMI',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
